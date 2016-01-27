2f896f4de954f21132dc44a9996f4c96b121fccb
Rely Ky <relyky@hotmail.com>
2016-01-28 00:01:53 +0800

    first commit

---
+++ winfrm_GetMouseCoordinates/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace winfrm_GetMouseCoordinates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(12, 9);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(35, 12);
+            this.labelX.TabIndex = 0;
+            this.labelX.Text = "labelX";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(12, 21);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(35, 12);
+            this.labelY.TabIndex = 1;
+            this.labelY.Text = "labelY";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 388);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+    }
+}
+
---
+++ winfrm_GetMouseCoordinates/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winfrm_GetMouseCoordinates
+{
+    public partial class Form1 : Form
+    {
+        private int preX = 0;
+        private int preY = 0;
+        private string preS = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // 此段原碼參考改良自：https://www.youtube.com/watch?v=YRcs4Rym_0Q
+            labelX.Text = e.X.ToString();
+            labelY.Text = e.Y.ToString();
+
+            Graphics g = this.CreateGraphics();
+
+            // erase
+            //g.DrawLine(Pens.White, 0, 0, preX, preY);
+            g.DrawLines(Pens.White, new Point[] { new Point(0, 0), new Point(preX, preY), new Point(this.Width, 0) });
+            g.DrawString(preS, this.Font, Brushes.White, preX, preY);
+
+            // draw
+            string s = string.Format("({0},{1})", e.X, e.Y);
+            int x = e.Location.X;
+            int y = (int)(e.Location.Y - this.Font.Size);
+            //g.DrawLine(Pens.Aqua, 0, 0, x, y);
+            g.DrawLines(Pens.Aqua, new Point[] { new Point(0, 0), new Point(x, y), new Point(this.Width, 0) });
+            g.DrawString(s, this.Font, Brushes.Blue, x, y);
+
+            // for next round
+            preX = x;
+            preY = y;
+            preS = s;
+        }
+
+    }
+}
